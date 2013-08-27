0271b9d16464c0a620c4f5ee05e7f0174a5645b5
beepscore <support@beepscore.com>
2013-08-26 23:09:49 -0700

    Add function_args_tests.js.
    To run unit tests:
    Start terminal
    cd node_practice/test
    $mocha .
    i.e. mocha "dot"

---
+++ test/function_args_tests.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var should = require("should");
+var function_args = require('../function_args');
+
+describe('function_args', function(){
+
+  describe('#addTwo', function(){
+
+    it('should return number incremented by 2', function() {
+      var x = 3;
+      var expectedResult = 5;
+      var actualResult = function_args.addTwo(x);
+      assert.equal(actualResult, expectedResult,
+             "expected " + expectedResult + " but got " + actualResult);
+    });
+
+  });
+
+});
+
