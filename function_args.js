bf2eb4c228632edcb00ddeda02bb2cdb991abbd9
beepscore <support@beepscore.com>
2013-08-26 21:43:38 -0700

    Add function addTwo().

--- function_args.js
+++ function_args.js
@@ -8,5 +8,14 @@ var putHello = function () {
     sys.puts("hello");
 }
 
+var addTwo = function (aNumber) {
+  return aNumber += 2;
+}
+
 putHello();
+
+var x = 3;
+sys.puts("addTwo(" + x + "): " + addTwo(x));
+sys.puts("x: " + x);
+
 
